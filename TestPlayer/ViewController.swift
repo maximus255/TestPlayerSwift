b10c8a80e6ad72f1f230fd1e174a173638f0c270
Maxim Chernousov <maxim.chernousov@gmail.com>
2021-05-11 14:48:05 +0300

    Initial Commit

---
+++ TestPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPlayer
+//
+//  Created by admin on 11.05.2021.
+//  Copyright © 2021 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
